3a7c20be1c2eadbd39d71eb80cf7d9283d90c193
Florian Fournier <florian.fournier63@gmail.com>
2022-12-05 09:47:22 +0100

    bottle

--- Wine celar/Entities/Drawer.cs
+++ Wine celar/Entities/Drawer.cs
@@ -4,7 +4,7 @@
     {
         public int DrawerId { get; set; }
         public int Index { get; set; }
-        public int NbBottle { get; set; }
+        public int NbBottleMax { get; set; }
         public Celar Celar { get; set; }
         public int CelarId { get; set; }
         public List<Wine> Wines { get; set; }
