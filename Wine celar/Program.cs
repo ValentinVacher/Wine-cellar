b2c6d89f5cb8364955ef6f68c5ceab14bd9d7c62
HB <camelobastien@hotmail.fr>
2022-12-05 10:10:19 +0100

    Connexion

--- Wine celar/Program.cs
+++ Wine celar/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<WineContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("WineCellarDbCS"));
+});
 
 var app = builder.Build();
 
