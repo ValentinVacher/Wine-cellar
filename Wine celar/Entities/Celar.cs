173b27924e9bf5d4da0c2fd1eb441d1c1828219f
valentin vacher <valentinvacher01@gmail.com>
2022-12-05 09:40:32 +0100

    class

--- Wine celar/Entities/Celar.cs
+++ Wine celar/Entities/Celar.cs
@@ -1,6 +1,14 @@
-﻿namespace Wine_celar.Entities
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Wine_celar.Entities
 {
     public class Celar
     {
+        public int CelarId {get;set;}
+        public string Name { get;set;}
+        public int NbDrawer { get;set;}
+        public int UserId { get;set;}
+        public User User { get;set;}
+        public List<Drawer> DrawerList { get;set;}
     }
 }
--- Wine celar/Entities/User.cs
+++ Wine celar/Entities/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Celar> CellarList { get; set; }
     }
 }
