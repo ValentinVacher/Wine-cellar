3b16134ff73ee0f0c401a5748041e54199c70d05
valentin vacher <valentinvacher01@gmail.com>
2022-12-05 13:31:10 +0100

    controller

---
+++ Wine celar/Controllers/CellarController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wine_celar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CellarController : ControllerBase
+    {
+    }
+}
