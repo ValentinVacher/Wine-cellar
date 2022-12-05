047d26de77cc75fafd4b7c0cdc4fb63622475eeb
Florian Fournier <florian.fournier63@gmail.com>
2022-12-05 10:38:09 +0100

    interface maj

--- Wine celar/IRepositories/ICelarRepository.cs
+++ Wine celar/IRepositories/ICelarRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Wine_celar.IRepositories
+﻿using Wine_celar.Entities;
+
+namespace Wine_celar.IRepositories
 {
     public interface ICelarRepository
     {
+        Task<List<Celar>> GetAlls();
+        Task<Celar> GetCelarWithAll(int id);
+        Task<Celar> DeleteCelar(int id);
+        Task <Celar> UpdateCelar(Celar celar);
     }
 }
