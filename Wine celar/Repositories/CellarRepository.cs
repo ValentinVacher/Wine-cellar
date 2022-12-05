6707238d979c2d6c007094af0d9274855cb123bf
HB <camelobastien@hotmail.fr>
2022-12-05 12:30:06 +0100

    les fautes

--- Wine celar/Repositories/CellarRepository.cs
+++ Wine celar/Repositories/CellarRepository.cs
@@ -1,6 +1,28 @@
-﻿namespace Wine_celar.Repositories
+﻿using Wine_cellar.Entities;
+using Wine_cellar.IRepositories;
+
+namespace Wine_celar.Repositories
 {
-    public class CellarRepository
+    public class CellarRepository : ICellarRepository
     {
+        public Task<Celar> DeleteCellarAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Cellar>> GetAllsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cellar> GetCellarWithAllAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cellar> UpdateCellarAsync(Cellar cellar)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
