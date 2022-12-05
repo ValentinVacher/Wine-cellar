c49376fd7828bd5a989a469e4ab3d5e571986938
HB <camelobastien@hotmail.fr>
2022-12-05 09:41:58 +0100

    Update

--- Wine celar/Entities/Wine.cs
+++ Wine celar/Entities/Wine.cs
@@ -2,5 +2,14 @@
 {
     public class Wine
     {
+        public int WineId { get; set; }
+        public string Color { get; set; }
+        public string Appelation { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int KeepMin { get; set; }
+        public int KeepMax { get; set;}
+        public Drawer Drawer { get; set; }
+        public int DrawerId { get; set; }
     }
 }
