31e087388c6203407ed9ae9cc3b5404dd7dd9eb9
HB <camelobastien@hotmail.fr>
2022-12-05 12:06:21 +0100

    Update

--- Wine celar/IRepositories/IWineRepository.cs
+++ Wine celar/IRepositories/IWineRepository.cs
@@ -7,7 +7,7 @@ namespace Wine_celar.IRepositories
         Task<List<Wine>> GetAllWinesAsync();
         Task<Wine> GetWineByIdAsync(int Wineid);
         Task<List<Wine>> GetWineByNameAsync(string name);
-        Task<List<Wine>> GetWineByColor(string color);
+        Task<List<Wine>> GetWineByColorAsync(string color);
         Task<Wine> CreateWineAsync(Wine wine);
         Task<Wine> UpdateWineAsync(Wine wine);
         Task<Wine> DeleteWineAsync(int WineId);
--- Wine celar/Repositories/wineRepository.cs
+++ Wine celar/Repositories/wineRepository.cs
@@ -1,6 +1,30 @@
-﻿namespace Wine_celar.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Wine_celar.Contexts;
+using Wine_celar.Entities;
+using Wine_celar.IRepositories;
+
+namespace Wine_celar.Repositories
 {
-    public class wineRepository
+    public class wineRepository : IWineRepository
     {
+        readonly WineContext wineContext;
+        ILogger logger;
+        public wineRepository(WineContext wineContext, ILogger logger)
+        {
+            this.wineContext = wineContext;
+            this.logger = logger;
+        }
+
+        public async Task<List<Wine>> GetAllWineAsync()
+        {
+            return await wineContext.Wines.ToListAsync();
+        }
+        public async Task<Wine> GetWineByIdAsync(int wineId)
+        {
+            return await wineContext.Wines.Include(p => p.Name).FirstOrDefaultAsync(p => p.WineId == wineId);
+        }
+
+        public
+
     }
 }
