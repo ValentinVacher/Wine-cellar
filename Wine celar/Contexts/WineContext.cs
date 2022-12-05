4a1ee5bd427b26899e867d1755123c71544f90f4
valentin vacher <valentinvacher01@gmail.com>
2022-12-05 10:59:11 +0100

    bdd

--- Wine celar/Contexts/WineContext.cs
+++ Wine celar/Contexts/WineContext.cs
@@ -24,5 +24,59 @@ namespace Wine_celar.Contexts
             }
             base.OnConfiguring(optionsBuilder);
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var c1 = new Celar { CelarId = 1, Name = "Clelars 1", NbDrawerMax = 5, UserId = 1 };
+            var c2 = new Celar { CelarId = 2, Name = "Clelars 2", NbDrawerMax = 10, UserId = 2 };
+            var c3 = new Celar { CelarId = 3, Name = "Clelars 3", NbDrawerMax = 20, UserId = 3 };
+
+            var d1 = new Drawer { DrawerId = 1, Index = 1, CelarId = 1 };
+            var d2 = new Drawer { DrawerId = 2, Index = 2, CelarId = 1 };
+            var d3 = new Drawer { DrawerId = 3, Index = 3, CelarId = 1 };
+            var d4 = new Drawer { DrawerId = 4, Index = 1, CelarId = 2 };
+            var d5 = new Drawer { DrawerId = 5, Index = 2, CelarId = 2 };
+            var d6 = new Drawer { DrawerId = 6, Index = 3, CelarId = 2 };
+
+            var v1 = new Wine { WineId = 1, Color = "Rosé", Appelation = "Appelation1", Name = "20-1", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 1};
+            var v2 = new Wine { WineId = 2, Color = "Bleu", Appelation = "Appelation2", Name = "20-2", Year = 1970, Today = DateTime.Now, KeepMin = 2001, KeepMax = 2002, DrawerId = 1 };
+            var v3 = new Wine { WineId = 3, Color = "Verre", Appelation = "Appelation3", Name = "20-3", Year = 1980, Today = DateTime.Now, KeepMin = 2001, KeepMax = 2002, DrawerId = 2 };
+            var v4 = new Wine { WineId = 4, Color = "Rouge", Appelation = "Appelation4", Name = "20-4", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 2 };
+            var v5 = new Wine { WineId = 5, Color = "Joune", Appelation = "Appelation5", Name = "20-5", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 3 };
+            var v6 = new Wine { WineId = 6, Color = "Blanc", Appelation = "Appelation6", Name = "20-6", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 3 };
+            var v7 = new Wine { WineId = 7, Color = "Rouge", Appelation = "Appelation7", Name = "20-7", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 4 };
+            var v8 = new Wine { WineId = 8, Color = "Violet", Appelation = "Appelation8", Name = "20-8", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 4 };
+            var v9 = new Wine { WineId = 9, Color = "Orange", Appelation = "Appelation9", Name = "20-9", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 5 };
+            var v10 = new Wine { WineId = 10, Color = "Violet", Appelation = "Appelation10", Name = "20-10", Year = 1960, Today = DateTime.Now, KeepMin = 2000, KeepMax = 2002, DrawerId = 5 };
+
+            var u1 = new User { UserId = 1, FirstName = "G", LastName = "G", Email = "test@test.com", Password = "test" };
+            var u2 = new User { UserId = 2, FirstName = "G2", LastName = "G2", Email = "test2@test.com", Password = "test2" };
+            var u3 = new User { UserId = 2, FirstName = "G3", LastName = "G3", Email = "test3@test.com", Password = "test3" };
+
+            c1.DrawerList.Add(d1);
+            c1.DrawerList.Add(d2);
+            c1.DrawerList.Add(d3);
+            c2.DrawerList.Add(d4);
+            c2.DrawerList.Add(d5);
+            c2.DrawerList.Add(d6);
+
+            d1.Wines.Add(v1);
+            d1.Wines.Add(v2);
+            d2.Wines.Add(v3);
+            d2.Wines.Add(v4);
+            d3.Wines.Add(v5);
+            d3.Wines.Add(v6);
+            d4.Wines.Add(v7);
+            d4.Wines.Add(v8);
+            d5.Wines.Add(v9);
+            d5.Wines.Add(v10);
+
+            modelBuilder.Entity<Celar>().HasData(new List<Celar> { c1, c2, c3 });
+            modelBuilder.Entity<Drawer>().HasData(new List<Drawer> { d1, d2, d3, d4, d5, d6 });
+            modelBuilder.Entity<Wine>().HasData(new List<Wine> { v1, v2, v3, v4, v5, v6, v7, v8, v9, v10 });
+            modelBuilder.Entity<User>().HasData(new List<User> { u1, u2, u3 });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
