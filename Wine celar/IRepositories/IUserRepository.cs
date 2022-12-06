29febcb10c31dfedac6aad680b59616f174ac981
HB <camelobastien@hotmail.fr>
2022-12-06 15:37:57 +0100

    Up

--- Wine celar/IRepositories/IUserRepository.cs
+++ Wine celar/IRepositories/IUserRepository.cs
@@ -4,10 +4,15 @@ namespace Wine_cellar.IRepositories
 {
     public interface IUserRepository
     {
+        //Permet de recuperer tout les users
         Task<List<User>> GetAllUserAsync();
+        //Permet de créer un user 
         Task<User> CreateUserAsync(User user);
+        //Permet de mettre à jour les infos du user
         Task<User> UpdateUserAsync(User user);
+        //Permet de supprimer un user
         Task<bool> DeleteUserAsync(int UserId);
+        //Permet de se connecter en tant que user
         Task<User> LoginUser(string login, string pwd);
     }
 }
