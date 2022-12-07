3417008fd7f9c70c8ec1b64be17d772878f7fc52
HB <camelobastien@hotmail.fr>
2022-12-07 15:14:36 +0100

    up

--- Wine celar/ViewModel/CreateWineViewModel.cs
+++ Wine celar/ViewModel/CreateWineViewModel.cs
@@ -2,12 +2,9 @@
 {
     public class CreateWineViewModel
     {
-        public string Color { get; set; }
-        public string Appelation { get; set; }
+        
         public string Name { get; set; }
         public int Year { get; set; }
-        public int KeepMin { get; set; }
-        public int KeepMax { get; set; }
         public int DrawerId { get; set; }
         public IFormFile Picture { get; set; }
     }
