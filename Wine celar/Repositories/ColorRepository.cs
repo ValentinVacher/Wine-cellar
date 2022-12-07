21b2eb65b23bd95b0524428c579a6a4f1283b827
Florian Fournier <florian.fournier63@gmail.com>
2022-12-07 13:57:01 +0100

    colorRepositories

--- Wine celar/Repositories/ColorRepository.cs
+++ Wine celar/Repositories/ColorRepository.cs
@@ -1,8 +1,28 @@
-﻿using Wine_cellar.IRepositories;
+﻿using Microsoft.EntityFrameworkCore;
+using Wine_cellar.Contexts;
+using Wine_cellar.Entities;
+using Wine_cellar.IRepositories;
 
-namespace Wine_celar.Repositories
+namespace Wine_cellar.Repositories
 {
     public class ColorRepository : IColorRepository
     {
+        WineContext winecontext;
+        ILogger<ColorRepository> logger;
+        public ColorRepository(WineContext winecontext, ILogger<ColorRepository> logger)
+        {
+            this.winecontext = winecontext;
+            this.logger = logger;
+        }
+
+        public async Task<List<ColorWine>> GetColorswithAppelationAsync()
+        {
+            return await winecontext.ColorWines.Include(c => c.Appelations).ToListAsync();
+        }
+
+        public async Task<ColorWine> GetColorwithAppelationByNameAsync(string ColorName)
+        {
+            return await winecontext.ColorWines.Include(c => c.Appelations).FirstOrDefaultAsync(c => c.Name == ColorName);
+        }
     }
 }
