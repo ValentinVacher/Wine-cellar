92dd027b4da76f8de9d49fdee6d2b87f432a34c6
valentin vacher <valentinvacher01@gmail.com>
2022-12-13 10:44:55 +0100

    dfg

--- Wine celar/Controllers/AppelationController.cs
+++ Wine celar/Controllers/AppelationController.cs
@@ -87,18 +87,18 @@ namespace Wine_celar.Controllers
         }
 
         [HttpDelete]
-        public async Task<IActionResult> DeleteAppelation(int appelationId)
+        public async Task<IActionResult> DeleteAppelation(int id)
         {
             var identity = User?.Identity as ClaimsIdentity;
 
             if (identity?.FindFirst(ClaimTypes.NameIdentifier) == null) return BadRequest(ErrorCode.UnLogError);
             if (identity?.FindFirst(ClaimTypes.Role).Value != "admin") return BadRequest(ErrorCode.NotAdminError);
 
-            var success = await AppelationRepository.DeleteAppelationAsync(appelationId);
+            var success = await AppelationRepository.DeleteAppelationAsync(id);
 
             if (success == 0) NotFound(ErrorCode.AppelationNotFound);
 
-            return Ok(appelationId);
+            return Ok(id);
         }
     }
 }
